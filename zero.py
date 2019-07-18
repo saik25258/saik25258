f4da9ed67bdb2db6726d50e848087d9b7853addd
saik25258 <53034321+saik25258@users.noreply.github.com>
2019-07-18 12:12:56 +0530

    Create zero.py

---
+++ zero.py
@@ -0,0 +1,7 @@
+a=int(input('enter the value of a'))
+if(a>0):
+  print('the given value is positive value')
+elif(a<0):
+  print('the given value is negative value')
+else:
+  print('the given value is zero')
